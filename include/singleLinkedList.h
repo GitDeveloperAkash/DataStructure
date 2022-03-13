fa946e790e9700c91be10fe9e690479ce87b023e
Aakash Vishvakarma <akashvishvakarma703@gmail.com>
2022-03-13 18:47:16 +0530

    added project

---
+++ include/singleLinkedList.h
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class NODE
+{
+public:
+
+    T data;
+    class NODE* next;
+    NODE(T data)
+    {
+        this->data = data;
+    }
+};
+
+
+
+
+
+template <class T>
+class singleLinkedList
+{
+    public:
+        singleLinkedList();
+        virtual ~singleLinkedList();
+
+        void PushBack(T val);
+        void PushFront(T val);
+        void Insert(int Pos, T val);
+        void display();
+        int getSize();
+
+    protected:
+        int SIZE;
+
+    private:
+        NODE<T> *Head;
+        NODE<T> *Tail;
+};
+
+template <class T>
+int singleLinkedList<T>::getSize()
+{
+    return SIZE;
+}
+
+template <class T>
+void singleLinkedList<T>::Insert(int Pos, T val)
+{
+    if(Pos < 1 || Pos > SIZE)
+    {
+        cout << "Invalid Position " << endl;
+        return;
+    }
+    NODE<T> *newNode;
+    newNode = new NODE<T>(val);
+    newNode->next = nullptr;
+
+    NODE<T> *current;
+    current = Head;
+
+    while(--Pos)
+        current = current->next;
+
+    if(current == Head)
+    {
+        newNode->next = Head;
+        Head = newNode;
+    }
+    else if(current == Tail)
+    {
+        Tail->next = newNode;
+        Tail = newNode;
+    }
+    else
+    {
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    SIZE++;
+}
+
+
+template <class T>
+void singleLinkedList<T>::PushBack(T val)
+{
+    NODE<T> *newNode;
+    newNode = new NODE<T>(val);
+    newNode->next = nullptr;
+
+    if(Tail)
+    {
+        Tail->next = newNode;
+        Tail = newNode;
+    }
+    else
+    {
+        Tail = newNode;
+        Head = Tail;
+    }
+    SIZE++;
+}
+
+template <class T>
+void singleLinkedList<T>::PushFront(T val)
+{
+    NODE<T> *newNode;
+    newNode = new NODE<T>(val);
+    newNode->next = nullptr;
+
+    if(Head)
+    {
+        newNode->next = Head;
+        Head = newNode;
+    }
+    else
+    {
+        Head = newNode;
+        Tail = Head;
+    }
+    SIZE++;
+}
+
+
+
+template <class T>
+void singleLinkedList<T>::display()
+{
+    struct NODE<T> *current;
+    current = Head;
+    while(current)
+    {
+        cout << current->data;
+        current = current->next;
+        if(current)
+            cout << " --> " ;
+    }
+    cout << endl;
+}
+
+template <class T>
+singleLinkedList<T>::singleLinkedList():Head(nullptr), Tail(nullptr),SIZE(0)
+{
+    cout << "constructor invoked " << endl;
+
+}
+
+template <class T>
+singleLinkedList<T>::~singleLinkedList()
+{
+    cout << "distructor invoked " << endl;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <singleLinkedList.h>
+using namespace std;
+
+int main()
+{
+    singleLinkedList<int> _list;
+
+    //_list.PushBack(10);
+    //_list.PushBack(20);
+    //_list.PushBack(30);
+    //_list.PushFront(80);
+    //_list.PushBack(40);
+    //_list.PushFront(70);
+    //_list.PushFront(90);
+
+    _list.Insert(1,50);
+    _list.display();
+
+
+    return 0;
+}
